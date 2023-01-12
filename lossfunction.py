Fixed Derivative of Tukey Loss

lossfunction.py
@@ -173,4 +173,4 @@ class Tukey():
 
     def backward(self, y_true: npt.NDArray[np.float64], y_predicted: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
         residual = y_true - y_predicted
-        return np.where(np.abs(residual) <= self.delta, residual * (1 - residual**2 / self.delta**2)**2, 1)
+        return - np.where(np.abs(residual) <= self.delta, residual * (1 - residual**2 / self.delta**2)**2, 1)