ce9f34741124ab21755bf15f283224e027d9c10c
QuantumAstronomer <t.essen@hotmail.com>
2023-01-12 10:19:40 +0100

    Modified the DecayFunction protocol

--- learningrate.py
+++ learningrate.py
@@ -16,89 +16,100 @@ from typing import Protocol
 
 class DecayFunction(Protocol):
 
-    learning_rate: float
-
+    def set_initial_learning_rate(self, initital_learning_rate: float):
+        ...
+        
     def calculate(self, iteration: int) -> float:
         ...
 
 
-class NoDecay():
+class NoDecay(DecayFunction):
     '''
     This is the class for no decay of the learning rate.
     '''
 
-    def __init__(self, initial_learning_rate: float):
-        self.learning_rate = initial_learning_rate
+    def set_initial_learning_rate(self, learning_rate: float):
+        self.learning_rate: float = learning_rate
 
     def calculate(self, iteration: int) -> float:
         return self.learning_rate
 
-class StandardDecay():
+class StandardDecay(DecayFunction):
     '''
     This is the most common decay function for the learning rate.
     '''
 
-    def __init__(self, initial_learning_rate: float, decay_rate: float):
-        self.learning_rate = initial_learning_rate
+    def __init__(self, decay_rate: float):
         self.decay_rate = decay_rate
+
+    def set_initial_learning_rate(self, learning_rate: float):
+        self.learning_rate: float = learning_rate
 
     def calculate(self, iteration: int) -> float:
         return self.learning_rate / (1 + self.decay_rate * iteration)
 
 
-class ExponentialDecay():
+class ExponentialDecay(DecayFunction):
     '''
     Exponential decay function for the learning rate.
     '''
 
     def __init__(self, initial_learning_rate: float, decay_rate: float):
-        self.learning_rate = initial_learning_rate
         self.decay_rate = decay_rate
+
+    def set_initial_learning_rate(self, learning_rate: float):
+        self.learning_rate: float = learning_rate
 
     def calculate(self, iteration: int) -> float:
         return self.learning_rate * self.decay_rate**iteration
 
 
-class DiscreteDecay():
+class DiscreteDecay(DecayFunction):
     '''
     Discrete standard decay function for the learning rate. Instead of continuously allowing the
     learning rate to decline, this function requires discrete steps.
     '''
 
-    def __init__(self, initial_learning_rate: float, decay_rate: float, decay_every_n_steps: int):
-        self.learning_rate = initial_learning_rate
+    def __init__(self, decay_rate: float, decay_every_n_steps: int):
         self.decay_rate = decay_rate
         self.decay_every_n_steps = decay_every_n_steps
+
+    def set_initial_learning_rate(self, learning_rate: float):
+        self.learning_rate: float = learning_rate
 
     def calculate(self, iteration: int) -> float:
         return self.learning_rate / (1 + self.decay_rate * (iteration // self.decay_every_n_steps))
 
 
-class SquareRootDecay():
+class SquareRootDecay(DecayFunction):
     '''
     The learning rate decays by the square root of the iteration number.
     '''
 
     def __init__(self, initial_learning_rate: float, decay_rate: float):
-        self.learning_rate = initial_learning_rate
         self.decay_rate = decay_rate
+
+    def set_initial_learning_rate(self, learning_rate: float):
+        self.learning_rate: float = learning_rate
 
     def calculate(self, iteration: int) -> float:
         return self.learning_rate * self.decay_rate / np.sqrt(iteration)
 
 
-class InversePowerDecay():
+class InversePowerDecay(DecayFunction):
     '''
     The learning rate decays by the 1 / iteration^power
     '''
 
-    def __init__(self, initial_learning_rate: float, decay_rate: float, power: float | int):
-        self.learning_rate = initial_learning_rate
+    def __init__(self, decay_rate: float, power: float | int):
         self.decay_rate = decay_rate
         self.power = power
 
         if self.power < 0:
             raise ValueError('The power must be non-negative.')
 
+    def set_initial_learning_rate(self, learning_rate: float):
+        self.learning_rate: float = learning_rate
+    
     def calculate(self, iteration: int) -> float:
         return self.learning_rate * self.decay_rate / iteration**self.power
