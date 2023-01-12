c4dc257f7e10d571781ac778efefe02226d0c65e
QuantumAstronomer <t.essen@hotmail.com>
2023-01-12 12:26:33 +0100

    Reorganized file and corrected AdaD optimizer

--- optimizer.py
+++ optimizer.py
@@ -44,7 +44,7 @@ class Optimizer(Protocol):
 
 
 # Optimizers that require the speicifcation of a decay function for the learning rate.
-class GradientDescent(Optimizer):
+class SGD(Optimizer):
     '''
     "Vanilla" gradient descent optimization algorithm with implementations in place to use
     a decaying learning rate as the iteration increases and to use momentum which is a parameter
@@ -85,7 +85,7 @@ class GradientDescent(Optimizer):
         self.learning_rate = self.learning_rate_function.calculate(self.iteration)
 
 
-class NesterovGradient(Optimizer):
+class NAG(Optimizer):
     '''
     Nesterov accelerated gradient descent algorithm. This algorithm differs from the typical
     stochastic gradient descent with momentum by first calculating the momentum step and then
@@ -115,7 +115,7 @@ class NesterovGradient(Optimizer):
         self.learning_rate = self.learning_rate_function.calculate(self.iteration)
 
 
-class AdaptiveGradient(Optimizer):
+class AdaG(Optimizer):
     '''
     Adaptive gradient descent, proposed by Duchi, Hazan and Singer in 2011
     (url: https://www.jmlr.org/papers/volume12/duchi11a/duchi11a.pdf), is more 
@@ -149,51 +149,17 @@ class AdaptiveGradient(Optimizer):
         self.iteration += 1
 
 
-class AdaptiveDelta(Optimizer):
+class RMS(Optimizer):
     '''
-    Adaptive Delta algorithm proposed by Zeiler 2012 (url: https://arxiv.org/abs/1212.5701).
-    Adaptive Delta is an extension of Adaptive Gradient that seeks to reduce its aggressive
-    (monotonically) decreasing learning rate. Rather than accumulating the past squared
-    gradients, it restricts the window of accumulated past gradients by defining a recursively
-    decaying average of the past squared gradients, i.e. past squared gradients are multiplied 
-    by a constant smaller than one at each iteration leading to an exponential moving average.
-    '''
-
-    def __init__(self, learning_rate: float = 0.05, rho: float = .95, epsilon: float = 1e-7):
-
-        self.learning_rate: float = learning_rate
-
-        self.rho: float = rho
-        self.epsilon: float = epsilon
-
-        self.iteration: int = 0
-
-    def update_parameters(self, layer: TrainableLayer):
-
-        if len(layer.momentum_weights.shape) != 3:
-            layer.momentum_weights = np.zeros(shape = (2, *layer.weights.shape), dtype = np.float64)
-            layer.momentum_biases = np.zeros(shape = (2, *layer.biases.shape), dtype = np.float64)
-
-        layer.momentum_weights[0] = self.rho * layer.momentum_weights[0] + (1 - self.rho) * layer.dweights**2
-        layer.momentum_biases[0] = self.rho * layer.momentum_biases[0] + (1 - self.rho) * layer.dbiases**2
-
-        layer.momentum_weights[1] = self.rho * layer.momentum_weights[1] + (1 - self.rho) * \
-                                  (self.learning_rate * layer.dbiases / (np.sqrt(layer.momentum_biases[0]) + self.epsilon))**2
-        layer.momentum_biases[1] = self.rho * layer.momentum_biases[1] + (1 - self.rho) * \
-                                  (self.learning_rate * layer.dbiases / (np.sqrt(layer.momentum_biases[0]) + self.epsilon))**2
-
-        layer.weights += - layer.dweights * (np.sqrt(layer.momentum_weights[1]**2) + self.epsilon) / (np.sqrt(layer.momentum_weights[0]) + self.epsilon)
-        layer.biases += - layer.dbiases * (np.sqrt(layer.momentum_biases[1]**2) + self.epsilon) / (np.sqrt(layer.momentum_biases[0]) + self.epsilon)
-
-    def post_update(self):
-        self.iteration += 1
-
-class RootMeanSquare(Optimizer):
-    '''
-    The Root Mean Squared Propagation optimizer is an unpublished optimizer method proposed by
-    Geoff Hinton in his lectures on neural networks and optimizer functions. RMS propagation and
-    Adaptive Delta have both been developed independently to resolve Adaptive Gradient's problem
-    of radically diminishing learning rates.
+    The Root Mean Squared Propagation optimizer is an optimizer method proposed by Geoff Hinton 
+    in his lectures on neural networks and optimizer functions. RMS propagation and Adaptive 
+    Delta have both been developed independently to resolve Adaptive Gradient's aggressive
+    (monotonically) decreasing learning rate. This issue is remedied by not using the sum
+    of past squared gradients, an ever increasing number, leading to infinitesimally small
+    effective learning rates, but rather using the exponential moving average: at each step
+    instead of adding the gradient squared, it adds the gradient squared multiplied by some
+    factor 1 - rho to the previous running average multiplied by a factor rho. This results
+    in an exponential moving average.
     '''
 
     def __init__(self, learning_rate: float = 0.005, rho: float = .95, epsilon: float = 1e-7):
@@ -217,7 +183,49 @@ class RootMeanSquare(Optimizer):
         self.iteration += 1
 
 
-class AdaptiveMomentum(Optimizer):
+class AdaD(Optimizer):
+    '''
+    Adaptive Delta algorithm proposed by Zeiler 2012 (url: https://arxiv.org/abs/1212.5701). Similar
+    in spirit to the RMS propagation optimizer, it eliminates the need for a learning rate entirely.
+    It does so by keeping track of the exponential moving average of past squared parameter updates
+    and rescales the update to the parameters accordingly. While in theory this is very efficient,
+    AdaD is sensitive to the softening parameter epsilon, included to avoid division by zero,
+    especially at the start of the training process when the exponential moving average of past
+    squared parameter updates is still zero. For this reason AdaD has a hard time gaining traction
+    right from the start and thus has been surpassed by more recent versions that do employ a 
+    traditional static learning rate.
+    '''
+
+    def __init__(self, rho: float = .95, epsilon: float = 1e-5):
+
+        self.rho: float = rho
+        self.epsilon: float = epsilon
+
+        self.iteration: int = 0
+
+    def update_parameters(self, layer: TrainableLayer):
+
+        if len(layer.momentum_weights.shape) != 3:
+            layer.momentum_weights = np.zeros(shape = (2, *layer.weights.shape), dtype = np.float64)
+            layer.momentum_biases = np.zeros(shape = (2, *layer.biases.shape), dtype = np.float64)
+
+        layer.momentum_weights[0] = self.rho * layer.momentum_weights[0] + (1 - self.rho) * layer.dweights**2
+        layer.momentum_biases[0] = self.rho * layer.momentum_biases[0] + (1 - self.rho) * layer.dbiases**2
+
+        update_weights = - layer.dweights * (np.sqrt(layer.momentum_weights[1]**2 + self.epsilon)) / (np.sqrt(layer.momentum_weights[0] + self.epsilon))
+        update_biases = - layer.dbiases * (np.sqrt(layer.momentum_biases[1]**2 + self.epsilon)) / (np.sqrt(layer.momentum_biases[0] + self.epsilon))
+
+        layer.weights += update_weights
+        layer.biases += update_biases
+
+        layer.momentum_weights[1] = self.rho * layer.momentum_weights[1] + (1 - self.rho) * update_weights**2
+        layer.momentum_biases[1] = self.rho * layer.momentum_biases[1] + (1 - self.rho) * update_biases**2
+
+    def post_update(self):
+        self.iteration += 1
+
+
+class AdaM(Optimizer):
     '''
     The Adaptive Momentum Estimation (AdaM), proposed by Kingma and Ba 2015 (url: https://arxiv.org/abs/1412.6980)
     optimizer is another method that computes adaptive learning rates for each parameter. In addition to storing 
@@ -252,7 +260,7 @@ class AdaptiveMomentum(Optimizer):
         momentum_weights_corrected = layer.momentum_weights[0] / (1 - self.beta1**(self.iteration + 1))
         momentum_biases_corrected = layer.momentum_biases[0] / (1 - self.beta1**(self.iteration + 1))
         memory_weights_corrected = layer.momentum_weights[1] / (1 - self.beta2**(self.iteration + 1))
-        memory_biases_corrected = layer.momentum_biases[1] / (1 -self.beta2**(self.iteration + 1))
+        memory_biases_corrected = layer.momentum_biases[1] / (1 - self.beta2**(self.iteration + 1))
 
         layer.weights += -self.learning_rate * momentum_weights_corrected / np.sqrt(memory_weights_corrected + self.epsilon)
         layer.biases += -self.learning_rate * momentum_biases_corrected / np.sqrt(memory_biases_corrected + self.epsilon)
@@ -261,7 +269,7 @@ class AdaptiveMomentum(Optimizer):
         self.iteration += 1
 
 
-class NesterovAdaptive(Optimizer):
+class NAdaM(Optimizer):
     '''
     Nesterov Adaptive Momentum Estimation (NAdaM) optimizer which is a hybrid between the AdaM optimizer
     and Nesterov Accelerated GD. Its spirit is identical to the AdaM optimizer, while its method for implemnting
@@ -300,7 +308,7 @@ class NesterovAdaptive(Optimizer):
         self.iteration += 1
 
 
-class AdaptiveMeanSquared(Optimizer):
+class AMS(Optimizer):
     '''
     Adaptive learning rate methods are the norm in training neural networks. However, they have a possiblity of not
     converging quick enough towards the optimal solution as at some point the learning rate becomes so small that
